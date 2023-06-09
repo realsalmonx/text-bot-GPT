cb5cb8285a7c1bc2d171a5c84782775df34e8e11
Simon <65342847+realsalmonx@users.noreply.github.com>
2023-06-10 00:23:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import openai
 
-openai.api_key = "Paste your api key here as a string!"
+openai.api_key = "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"
 
 # Add a path to the directory where send_imessage.scpt is stored. This is most likely in the same directory as the rest of the downloaded files
-script_path = "/Path/to/directory/send_iMessage.scpt"
+script_path = "~/text-bot-GPT/send_imessage.scpt"
 
 # Change to False if you want the program to automatically send text messages without you looking it over
-require_approval = True
+require_approval = False
 
 # Paste the phone number you want to text with here as a string!
-phone_number = "+123456789"
+phone_number = "+84385788925"
